9db93b27833f0b861012874dbacf5a18ea66c9ca
odomontois <odomontois@gmail.com>
2018-06-11 22:29:05 +0300

    eq

--- src/main/scala/fpspeedrun/Eq.scala
+++ src/main/scala/fpspeedrun/Eq.scala
@@ -1,5 +1,33 @@
 package fpspeedrun
+import syntax.eq._
+
+import scala.annotation.tailrec
 
 trait Eq[T] {
   def ===(x: T, y: T): Boolean
+}
+
+object Eq {
+  def fromEquals[T]: Eq[T] = _ == _
+
+  implicit val intEq: Eq[Int] = fromEquals
+  implicit val longEq: Eq[Long] = fromEquals
+
+  /** простой вариант */
+  implicit def vectorEq[A: Eq]: Eq[Vector[A]] =
+    (xs, ys) => xs.view.zip(ys).forall { case (x, y) => x === y }
+
+
+  /** я у мамы оптимизатор */
+  implicit def listEq[A: Eq]: Eq[List[A]] = {
+    @tailrec def go(xs: List[A], ys: List[A]): Boolean =
+      xs match {
+        case Nil => ys.isEmpty
+        case x :: xt => ys match {
+          case Nil => false
+          case y :: yt => x === y && go(xt, yt)
+        }
+      }
+    go
+  }
 }
--- src/main/scala/fpspeedrun/Ratio.scala
+++ src/main/scala/fpspeedrun/Ratio.scala
@@ -1,4 +1,10 @@
 package fpspeedrun
 
-final case class Ratio()
+import syntax.eq._
+
+final case class Ratio(num: Int, den: Int)
+
+object Ratio {
+  implicit val eq: Eq[Ratio] = (x, y) => x.num.toLong * y.den === x.den.toLong * y.num
+}
 
