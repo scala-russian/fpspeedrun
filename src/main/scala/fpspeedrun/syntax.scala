c2fd51441d50e2736cc0822b9eb553db3105655d
odomontois <odomontois@gmail.com>
2018-06-11 20:53:27 +0300

    eq

---
+++ src/main/scala/fpspeedrun/syntax.scala
@@ -0,0 +1,11 @@
+package fpspeedrun
+
+object syntax {
+
+  object eq{
+    implicit class EqOps[T](val x: T) extends AnyVal{
+      def ===(y: T)(implicit eq: Eq[T]): Boolean = eq.===(x, y)
+    }
+  }
+
+}
