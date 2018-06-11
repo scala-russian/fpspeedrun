61f3de6980b92e6fd0d8c686f666587197ef72de
odomontois <odomontois@gmail.com>
2018-06-11 21:30:44 +0300

    ord

---
+++ src/main/scala/fpspeedrun/Ord.scala
@@ -0,0 +1,15 @@
+package fpspeedrun
+import fpspeedrun.Ord.Compare
+
+trait Ord[T] extends Eq[T]{
+  def compare(x: T, y: T): Compare
+}
+
+object Ord{
+  sealed trait Compare
+  object Compare{
+    case object LT //less than
+    case object EQ //equals to
+    case object GT //greater than
+  }
+}
--- src/main/scala/fpspeedrun/syntax.scala
+++ src/main/scala/fpspeedrun/syntax.scala
@@ -1,11 +1,9 @@
 package fpspeedrun
 
 object syntax {
-
   object eq{
     implicit class EqOps[T](val x: T) extends AnyVal{
       def ===(y: T)(implicit eq: Eq[T]): Boolean = eq.===(x, y)
     }
   }
-
 }
