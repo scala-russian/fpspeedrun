0c4ebaa62a91e8860b595e4e1a3ca523f3fb55cd
Nikita Filimonov <holypics6@gmail.com>
2018-06-16 18:48:14 +0500

    Some little improvements

--- src/main/scala/fpspeedrun/Ratio.scala
+++ src/main/scala/fpspeedrun/Ratio.scala
@@ -1,25 +1,14 @@
 package fpspeedrun
 
-<<<<<<< HEAD
-import syntax.eq._
-
-final case class Ratio(num: Int, den: Int)
-
-object Ratio {
-  implicit val eq: Eq[Ratio] = (x, y) => x.num.toLong * y.den === x.den.toLong * y.num
-}
-=======
 import fpspeedrun.Ord.Compare._
->>>>>>> FirstImpl
 
 final case class Ratio(numer: Int, denom: Int)
 
 object Ratio {
-  implicit val eqRatio: Eq[Ratio] =
-    (x: Ratio, y: Ratio) => x.numer * y.denom == y.numer * x.denom
 
   implicit val ordRatio: Ord[Ratio] = new Ord[Ratio] {
-    override def ===(x: Ratio, y: Ratio): Boolean = eqRatio.===(x, y)
+    override def ===(x: Ratio, y: Ratio): Boolean =
+      x.numer * y.denom == y.numer * x.denom
 
     override def compare(x: Ratio, y: Ratio): Ord.Compare =
       if (===(x, y)) EQ
--- src/main/scala/fpspeedrun/syntax/compare.scala
+++ src/main/scala/fpspeedrun/syntax/compare.scala
@@ -1,7 +1,21 @@
 package fpspeedrun.syntax
 
+import fpspeedrun.Ord
+import fpspeedrun.Ord.Compare.{EQ, GT, LT}
+
 object compare {
-    implicit class CompareOps[T](val x: T) extends AnyVal {
-      def compare(y: T)(implicit ord: Ord[T]): Ord.Compare = ord.compare(x, y)
+  implicit class CompareOps[T](val x: T) extends AnyVal {
+    def compare(y: T)(implicit ord: Ord[T]): Ord.Compare = ord.compare(x, y)
+
+    def >(y: T)(implicit ord: Ord[T]): Boolean = compare(y) == GT
+    def <(y: T)(implicit ord: Ord[T]): Boolean = compare(y) == LT
+    def >=(y: T)(implicit ord: Ord[T]): Boolean = {
+      val c = compare(y)
+      c == GT || c == EQ
+    }
+    def <=(y: T)(implicit ord: Ord[T]): Boolean = {
+      val c = compare(y)
+      c == LT || c == EQ
     }
   }
+}
