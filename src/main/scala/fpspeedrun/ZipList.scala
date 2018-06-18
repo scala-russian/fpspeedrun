f9acdb883a8abead3b050d6d0b57d76b0dfb45ac
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2018-06-18 15:33:39 +0300

    hw2 wip

--- src/main/scala/fpspeedrun/ZipList.scala
+++ src/main/scala/fpspeedrun/ZipList.scala
@@ -1,4 +1,5 @@
 package fpspeedrun
+import fpspeedrun.Ord.Compare.EQ
 import syntax.eq._
 import syntax.ord._
 
@@ -20,9 +21,14 @@ object ZipList {
 
   implicit def zipListMonoid[A: Monoid]: Monoid[ZipList[A]] = ???
 
-  implicit def zipListEq[A: Eq]: Eq[ZipList[A]] = ???
+  implicit def zipListEq[A: Eq]: Eq[ZipList[A]] = _.value === _.value
 
-  implicit def zipListOrd[A: Ord]: Ord[ZipList[A]] = ???
+  implicit def zipListOrd[A: Ord]: Ord[ZipList[A]] = {
+    case (Repeat(x), Repeat(y)) => x compare y
+    case (Repeat(x), Final(y)) => y.view.map(x compare _).find(_ =/= EQ).getOrElse(EQ)
+    case (Final(x), Repeat(y)) => x.view.map(_ compare y).find(_ =/= EQ).getOrElse(EQ)
+    case (Final(x), Final(y)) => x compare y
+  }
 
   implicit def zipListNum[A: Num]: Num[ZipList[A]] = ???
 
