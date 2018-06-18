4e5561093fd436355c40d99ea0342ee68777474f
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2018-06-18 16:02:09 +0300

    hw2 wip

--- src/main/scala/fpspeedrun/ZipList.scala
+++ src/main/scala/fpspeedrun/ZipList.scala
@@ -24,17 +24,28 @@ object ZipList {
 
   implicit def zipListEq[A: Eq]: Eq[ZipList[A]] = _.value === _.value
 
-  implicit def zipListOrd[A: Ord]: Ord[ZipList[A]] = {
-    case (Repeat(x), Repeat(y)) => x compare y
-    case (Repeat(x), Finite(y)) => y.view.map(x compare _).find(_ =/= EQ).getOrElse(EQ)
-    case (Finite(x), Repeat(y)) => x.view.map(_ compare y).find(_ =/= EQ).getOrElse(EQ)
-    case (Finite(x), Finite(y)) => x compare y
-  }
+  implicit def zipListOrd[A: Ord]: Ord[ZipList[A]] = new ZipListOrd[A]
 
   implicit def zipListNum[A: Num]: Num[ZipList[A]] = ???
 
   implicit def zipListInteg[A: Integ]: Integ[ZipList[A]] = ???
 
   implicit def zipListFrac[A: Frac]: Frac[ZipList[A]] = ???
+
+  class ZipListOrd[A: Ord] extends Ord[ZipList[A]] {
+    override def compare(xs: ZipList[A], ys: ZipList[A]): Ord.Compare = (xs, ys) match {
+      case (Repeat(x), Repeat(y)) => x compare y
+      case (Repeat(x), Finite(y)) => y.view.map(x compare _).find(_ =/= EQ).getOrElse(EQ)
+      case (Finite(x), Repeat(y)) => x.view.map(_ compare y).find(_ =/= EQ).getOrElse(EQ)
+      case (Finite(x), Finite(y)) => x compare y
+    }
+  }
+
+  class ZipListNum[A: Num] extends Num[ZipList[A]]{
+    override def fromInt(x: Int): ZipList[A] = repeat(x)
+    override def plus(x: ZipList[A], y: ZipList[A]): ZipList[A] =
+    override def times(x: ZipList[A], y: ZipList[A]): ZipList[A] = ???
+    override def compare(x: ZipList[A], y: ZipList[A]): Ord.Compare = ???
+  }
 
 }
