a896616126ec4b3dd0b4d2be05287b9fd89ea1d6
Alexey Suslov <alexei.suslov@gmail.com>
2018-06-21 00:21:43 +0300

    add Ord[Ratio] to companion object

--- src/main/scala/fpspeedrun/Ratio.scala
+++ src/main/scala/fpspeedrun/Ratio.scala
@@ -1,10 +1,16 @@
 package fpspeedrun
 
-import syntax.eq._
+import fpspeedrun.syntax.eq._
 
 final case class Ratio(num: Int, den: Int)
 
 object Ratio {
-  implicit val eq: Eq[Ratio] = (x, y) => x.num.toLong * y.den === x.den.toLong * y.num
+  //  implicit val eq: Eq[Ratio] = (x, y) => x.num.toLong * y.den === x.den.toLong * y.num
+  implicit val ord: Ord[Ratio] = new Ord[Ratio] {
+    override def compare(x: Ratio, y: Ratio): Ord.Compare = ???
+
+    override def ===(x: Ratio, y: Ratio): Boolean =
+      x.num.toLong * y.den === x.den.toLong * y.num
+  }
 }
 
